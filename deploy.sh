94611c2d0e939068f8002217c46b0da742ea7528
Zeqian Li <zeqianli.bio@gmail.com>
2025-04-19 16:47:47 -0700

    delete the past website

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-conda activate loconotion
-python loconotion/loconotion site.toml
-rm docs/*
-cp -R dist/zeqianli.me/* docs
-echo "zeqianli.me" > docs/CNAME
-
-git add .
-git commit -m "Deployments"
-git push origin main
